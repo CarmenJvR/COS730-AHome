ee0acf4217f92a0f81be08941805d15299ea4350
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-05-28 12:59:20 +0200

    schedule service added

---
+++ src/app/service/event.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient,  HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+export class eventService
+{
+    constructor(private httpClient : HttpClient , private router: Router){}
+
+    createEvent(PostData): Observable<any>{
+        const options = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }) 
+        };
+        return this.httpClient.post('https://a-home.herokuapp.com/createEvent', PostData, options );
+      }
+
+      removeEvent(PostData): Observable<any>{
+        const options = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }) 
+        };
+        return this.httpClient.post('https://a-home.herokuapp.com/removeEvent', PostData, options );
+      }
+
+      getSchedule(PostData): Observable<any>{
+        const options = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          }) 
+        };
+        return this.httpClient.post('https://a-home.herokuapp.com/scheduleList', PostData, options );
+      }
+
+  
+
+}

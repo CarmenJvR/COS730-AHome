49f6f0284d6a2771c494bdd953f15d64989d0ebe
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-06-03 11:19:12 +0200

    schedule tests initialised

--- src/app/examples/schedule/schedule.component.spec.ts
+++ src/app/examples/schedule/schedule.component.spec.ts
@@ -1,25 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { ScheduleComponent } from './schedule.component';
+import { AppModule } from '../../app.module';
+import { HttpTestingController , HttpClientTestingModule } from '@angular/common/http/testing';
+import { eventService } from '../../service/event.service';
+import { AppComponent } from 'app/app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+class MockEventService extends eventService {
+  // mock everything used by the component
+};
+
+
+  var test1 ={
+    pid: 3
+  }
 
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
   let fixture: ComponentFixture<ScheduleComponent>;
+  let evtService: eventService;
+  let httpMock: HttpTestingController;
+  let router: Router;
+  let http: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ScheduleComponent ]
+      imports: [RouterTestingModule.withRoutes([]) , HttpClientTestingModule, AppModule],
+      declarations: [ ScheduleComponent ],
+      providers: [{
+        provide: eventService,
+        useClass: MockEventService
+      }]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(ScheduleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    evtService = TestBed.get(eventService);
+    router = TestBed.get(Router);
+    httpMock = TestBed.get(HttpTestingController);
+    http = TestBed.get(HttpClient);
   });
 
-  it('should create', () => {
+
+
+  it('Successful creation of Schedule Component', () => {
     expect(component).toBeTruthy();
   });
 });

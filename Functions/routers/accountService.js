53c6adcae2e5f41a43a3847bb734746fde9d78b9
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-06-10 11:10:21 +0200

    schedule list

--- Functions/routers/accountService.js
+++ Functions/routers/accountService.js
@@ -507,6 +507,28 @@ router.post('/createEvent', async (req, res) => {
   }
 }); 
 
+//API: Get Schedule List
+router.post('/scheduleList', async (req, res) => {
+  try {
+    const client = await pool.connect();
+
+    const valuesR1 = [req.body.pid]
+
+    client.query('SELECT * FROM schedule WHERE project_id = $1', valuesR1 ,(error, results) => {
+      if (error) {
+       throw error
+      }
+      
+        const respond = { 'results': (results) ? results.rows : null};
+        res.send(JSON.stringify(respond));
+      })
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 
 module.exports = router
 
